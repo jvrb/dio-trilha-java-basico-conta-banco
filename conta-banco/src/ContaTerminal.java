8081e5a09fb4a2d20ed85997c1975a44377fddfb
jvrb <j.vgms2@hotmail.com>
2024-05-08 11:33:15 -0300

    feat: iniciando o projeto

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,9 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+    }
+}
